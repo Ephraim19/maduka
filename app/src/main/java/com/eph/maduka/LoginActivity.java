13670372b3207fc99b978df7f7d453b61215b56f
Ephraim19 <machayoephraim@gmail.com>
2022-04-24 10:03:01 +0300

    Added home page design

--- app/src/main/java/com/eph/maduka/LoginActivity.java
+++ app/src/main/java/com/eph/maduka/LoginActivity.java
@@ -24,6 +24,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int RC_SIGN_IN = 1001;
